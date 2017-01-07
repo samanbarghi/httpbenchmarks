a0d45228517ce60fdfd131fef1ec4023a4f88cfa
Saman Barghi <saman.b@gmail.com>
2017-01-07 14:00:49 -0500

    Updated the fasthttp code

--- fasthttp/server.go
+++ fasthttp/server.go
@@ -1,23 +1,55 @@
-type MyHandler struct {
-  foobar string
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	addr     = flag.String("addr", ":8080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
 
-// request handler in net/http style, i.e. method bound to MyHandler struct.
-func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
-  // notice that we may access MyHandler properties here - see h.foobar.
-  fmt.Fprintf(ctx, "Hello, world!")
-}
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-// request handler in fasthttp style, i.e. just plain function.
-func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-  fmt.Fprintf(ctx, "Hi there! RequestURI is %q", ctx.RequestURI())
-}
+	/*fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
-// pass bound struct method to fasthttp
-myHandler := &MyHandler{
-  foobar: "foobar",
-}
-fasthttp.ListenAndServe(":8800", myHandler.HandleFastHTTP)
+	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request) */
 
-// pass plain function to fasthttp
-//fasthttp.ListenAndServe(":8081", fastHTTPHandler)
+	ctx.SetContentType("text/plain; charset=utf8")
+
+	// Set arbitrary headers
+//	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+	// Set cookies
+/*	var c fasthttp.Cookie
+	c.SetKey("cookie-name")
+	c.SetValue("cookie-value")
+	ctx.Response.Header.SetCookie(&c) */
+}
